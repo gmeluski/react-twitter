29f91833c053a2a075460930f0944b555f562302
gmeluski <gmeluski@hotmail.com>
2015-02-21 15:58:19 -0500

    adding in more parts to the Tweets componenet

--- components/TweetsApp.react.js
+++ components/TweetsApp.react.js
@@ -21,4 +21,52 @@ module.exports = TweetsApp = React.createClass({
       done: false
     };
   },
+
+  componentWillReceiveProps: function (newProps, oldProps) {
+    this.setState(this.getInitialState(newProps));
+  },
+
+  componentDidMount: function () {
+    var self = this;
+    var socket = io.connect();
+
+    socket.on('tweet', function (data) {
+      self.addTweet(data);
+    });
+
+    window.addEventListener('scroll', this.checkWindowScroll);
+  },
+
+  addTweet: function (tweet) {
+    var updated = this.state.tweets;
+    var count = this.state.count + 1;
+    var skip = this.state.skip + 1;
+
+    updated.unshift(tweet);
+    this.setState({ tweets: updated, count: count, skip: skip });
+  },
+  
+  showNewTweets: function () {
+    var updated = this.state.tweets;
+
+    updated.forEach(function(tweet){
+      tweet.active = true; 
+    });
+
+    this.setState({ tweets: updated, count: 0});
+  },
+
+  checkWindowScroll: function () {
+    var h = Math.max(document.documentElement.clientHeight,
+      window.innerHeight || 0);
+
+    var s = document.body.scrollTop;
+    var scrolled = (h + s) > document.body.offsetHeight;
+
+    if (scrolled && !this.state.paging && !this.state.done) {
+      
+      this.setState({ paging: true, page: this.state.page + 1});
+      this.getPage(this.state.page);
+    }
+  },
 });

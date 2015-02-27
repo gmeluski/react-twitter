e276c054796facc74ccd0ed9301daa8d50b335da
Gregg Meluski <gmeluski@hotmail.com>
2015-02-27 16:18:32 -0500

    adding in more components

---
+++ components/Tweets.react.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var Tweet = require('./Tweet.react.js');
+
+module.exports = Tweets = React.createClass({
+
+  render: function () {
+
+    var content = this.props.tweet.map(function (tweet) {
+      return (
+        <Tweet key={tweet.twid} tweet={tweet} />
+      )
+    });
+
+    return (
+      <ul className='tweets'>{content}</ul>
+    )
+  },
+
+});
